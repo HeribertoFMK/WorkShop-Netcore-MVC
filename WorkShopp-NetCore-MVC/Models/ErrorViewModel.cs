d10b93a695225fddf4d2466d94e84a92df25970d
Heriberto F M Komatu <hfmk2015@gmail.com>
2021-08-16 19:55:21 +0900

    Iniciando os estudos sobre MVC

---
+++ WorkShopp-NetCore-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WorkShopp_NetCore_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
